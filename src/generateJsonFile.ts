e207ec35fc09aba07adbe9fd4532751fdfdeaefa
kobakakzu0429 <kobakazu10@gmail.com>
2021-05-01 11:53:05 +0900

    fix

--- src/generateJsonFile.ts
+++ src/generateJsonFile.ts
@@ -2,15 +2,16 @@ import * as path from "path";
 import { promises as fs } from "fs";
 import { parser } from "./parser";
 
-export async function generateJsonFile(
-  data: BufferSource,
-  outputFilename: string
-) {
-  const result = await parser(data);
-  const outputFilepath = path.resolve(
-    __dirname,
-    `../menu-json/${outputFilename}`
-  );
+export async function generateJsonFile(data: Buffer, outputFilename: string) {
+  try {
+    const result = await parser(data);
+    const outputFilepath = path.resolve(
+      __dirname,
+      `../menu-json/${outputFilename}`
+    );
 
-  await fs.writeFile(outputFilepath, JSON.stringify(result, null, 2));
+    await fs.writeFile(outputFilepath, JSON.stringify(result, null, 2));
+  } catch (error) {
+    console.log("called", error);
+  }
 }
